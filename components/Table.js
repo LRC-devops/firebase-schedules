73d4bb0e36c7dd0dc1f7d9240595d543502dc43b
ctheil <caleb.theil@ucdenver.edu>
2022-05-19 13:47:12 -0600

    clean up bad code

--- components/Table.js
+++ components/Table.js
@@ -4,52 +4,46 @@ import { doc } from "firebase/firestore";
 import TableRow from "../components/TableRow";
 
 export default function Table(props) {
-  const { action } = props;
-
-  console.log(props.posts);
-  const posts = props.posts;
-
-  console.log(posts);
-  const mapped = posts.map((session) => console.log(session.course));
+  const { posts, action } = props;
 
   // return mapped;
 
-  // if (action === "filteredAgSched") {
-  //   return (
-  //     <table key={String(Math.random())}>
-  //       {posts.map((session) => (
-  //         <tbody key={String(Math.random())}>
-  //           <tr key={session.course}>
-  //             <th colSpan={3}>{session[0].course}</th>
-  //           </tr>
-  //           {session.map((post) => (
-  //             <TableRow
-  //               key={doc.id}
-  //               post={post}
-  //               triggerModal={props.triggerModal}
-  //               dataId={doc.id}
-  //               isDeleted={props.isDeleted}
-  //               triggerEdit={props.triggerEdit}
-  //               radioHandler={props.radioHandler}
-  //               action={action}
-  //             />
-  //           ))}
-  //         </tbody>
-  //       ))}
-  //     </table>
-  //   );
-  // } else {
-  //   return posts.map((post) => (
-  //     <TableRow
-  //       key={doc.id}
-  //       post={post}
-  //       triggerModal={props.triggerModal}
-  //       dataId={doc.id}
-  //       isDeleted={props.isDeleted}
-  //       triggerEdit={props.triggerEdit}
-  //       radioHandler={props.radioHandler}
-  //       action={props.action}
-  //     />
-  //   ));
-  // }
+  if (action === "filteredAgSched") {
+    return (
+      <table key={String(Math.random())}>
+        {posts.map((session) => (
+          <tbody key={String(Math.random())}>
+            <tr key={session.course}>
+              <th colSpan={3}>{session[0].course}</th>
+            </tr>
+            {session.map((post) => (
+              <TableRow
+                key={doc.id}
+                post={post}
+                triggerModal={props.triggerModal}
+                dataId={doc.id}
+                isDeleted={props.isDeleted}
+                triggerEdit={props.triggerEdit}
+                radioHandler={props.radioHandler}
+                action={action}
+              />
+            ))}
+          </tbody>
+        ))}
+      </table>
+    );
+  } else {
+    return posts.map((post) => (
+      <TableRow
+        key={doc.id}
+        post={post}
+        triggerModal={props.triggerModal}
+        dataId={doc.id}
+        isDeleted={props.isDeleted}
+        triggerEdit={props.triggerEdit}
+        radioHandler={props.radioHandler}
+        action={props.action}
+      />
+    ));
+  }
 }
--- pages/User.js
+++ pages/User.js
@@ -15,62 +15,34 @@ const User = (props) => {
     setShowModal(false);
   };
 
-  const getData = async (btnName) => {
-    try {
-      const docRef = firestore
-        .collection("LRC")
-        .doc("schedules")
-        .collection("agSched")
-        .where("subject", "==", btnName);
-      const sessions = (await docRef.get()).docs.map(sessionToJSON);
-      // console.log("inside getData fn", sessions);
-      setModalContent({
-        sessions: sessions,
-      });
-      return {
-        props: { sessions },
-      };
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  // console.log(getData(props.modalContent.btnName));
-
   const btnClickHandler = (e) => {
     const btnName = e.target.innerHTML;
     setShowModal(true);
-    const sessions = getData(btnName);
     setModalContent({
       btnName: btnName,
-      // sessions: sessions,
     });
     console.log(modalContent.sessions);
-    // queryByBtnName(btnName);
     return btnName;
   };
 
-  // const {}useContext(SessionContext)
   const posts = props.posts;
-  // const posts = [modalContent.sessions];
   return (
     <main>
       <div className="flex-col">
         <div className="flex">
           <div className="table--box">
-            {/* <h1>Guided Study Groups</h1> */}
             <div>
               {showModal && (
                 <ScheduleModal
                   onClose={onCloseModal}
                   action="USER_ACCESS"
-                  // posts={posts}
+                  posts={posts}
                   btnName={btnClickHandler}
                   modalContent={modalContent}
                 />
               )}
               <UiCard />
               <ScheduleBtns posts={posts} btnClickHandler={btnClickHandler} />
-              {/* <Table key={String(Math.random())} posts={posts} /> */}
             </div>
           </div>
         </div>
